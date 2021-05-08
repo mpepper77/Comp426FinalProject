50758fe99f4945481931641ea64536a8f9d556cd
mollie pepper <mpepper@live.unc.edu>
2021-05-08 03:48:23 -0400

    newsapi

---
+++ public/signin.js
@@ -0,0 +1,12 @@
+// export const setUp = function(){
+//     const $root = $('#login');
+//     let toApp = `<p> Please sign in using your google login credentials</p>
+//             <div class="g-signin2" data-onsuccess="onSignIn"></div>`
+//     $root.append(toApp)
+//     console.log("hi")
+ 
+// }
+
+// $(function(){
+//     setUp();
+// });
